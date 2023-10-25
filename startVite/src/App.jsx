3bdc1707e2e83f692234040dbbfa4ca7652efc34
João Vitor Alves Ribeiro <joao.ribeiro@phb.com>
2023-10-25 16:51:50 -0300

    start

---
+++ startVite/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <h1 className='eba'>Helo word</h1>
+
+     <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing 
+      elit. Placeat velit esse accusamus ea et eveniet n
+      obis ipsa perspiciatis exercitationem. Placeat omnis 
+      rem, laborum eligendi non incidunt inventore deserunt. 
+      Optio, atque.
+      </p>
+    </>
+  )
+}
+
+export default App
